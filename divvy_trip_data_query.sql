d0fe7d107898b559277b541e4fea04ed67cb2392
gaphalpa <88202526+gaphalpa@users.noreply.github.com>
2021-09-30 18:37:26 -0500

    Update divvy_trip_data_query.sql

--- divvy_trip_data_query.sql
+++ divvy_trip_data_query.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS dbo.divvy_trip_data
 SELECT
 	*,
-	DATEDIFF(MINUTE,started_at,ended_at) AS trip_length, -- Creates 'trip_length' column that returns the duration of the trip in minutes
-	DATENAME(dw,started_at) AS day_of_week, -- Creates 'day_of_week' column that returns the day when the trip occurred
-	Geography::Point(start_lat,start_lng,4326).STDistance(Geography::Point(end_lat,end_lng,4326)) AS trip_distance -- Creates 'trip_distance' column that returns the distance traveled by rider
+	DATEDIFF(MINUTE,started_at,ended_at) AS trip_length, -- Returns the duration of the trip
+	DATENAME(dw,started_at) AS day_of_week, -- Returns the day when the trip occurred
+	Geography::Point(start_lat,start_lng,4326).STDistance(Geography::Point(end_lat,end_lng,4326)) AS trip_distance -- Returns the distance traveled
 INTO
 	divvy_trip_data -- Compiled and cleaned table for analysis
 FROM
@@ -202,20 +202,8 @@ FROM
 	FROM dbo.tripdata202108
 ) as tripdata_all
 WHERE
-	start_station_name IS NOT NULL -- This excludes trips that do not have data on starting location and are thus incomplete trips
-	AND end_station_name IS NOT NULL -- This excludes trips that do not have data on ending location and are thus incomplete trips
-	AND start_station_name <> end_station_name -- This excludes trips that have the same starting and ending location, which means that they are not comlpleted trips
-	AND DATEDIFF(MINUTE,started_at,ended_at) > 0 -- This excludes trips that have a starting time that is later than the ending time, meaning they are dirty data
-	AND DATEDIFF(MINUTE,started_at,ended_at) < 60 * 24 -- This excludes trips that lasted for more than 24 hours which are prohibited by the app
-
--- This query selects relevant columns to be used for visualization
-SELECT
-	member_casual AS rider_type,
-	started_at AS start_time,
-	ended_at AS end_time,
-	start_station_name AS start_station,
-	end_station_name AS end_station,
-	trip_length,
-	day_of_week,
-	ROUND(trip_distance,0) AS trip_distance -- The ROUND function is used to improve readability in visualization
-FROM divvy_trip_data
+	start_station_name IS NOT NULL -- This excludes trips that do not have data on starting location
+	AND end_station_name IS NOT NULL -- This excludes trips that do not have data on ending location
+	AND start_station_name <> end_station_name -- This excludes trips that have the same starting and ending location
+	AND DATEDIFF(MINUTE,started_at,ended_at) > 0 -- This excludes trips that have a starting time that is later than the ending time
+	AND DATEDIFF(MINUTE,started_at,ended_at) < 60 * 24 -- This excludes trips that lasted for more than 24 hours
